7a498302a7011ad6f76428192132031f2eaecf3b
Vieetj-Anh <hovietanh2812@gmail.com>
2026-03-25 23:29:01 +0700

    first commit

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,17 @@
+package $org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2,3));
+    }
+}
